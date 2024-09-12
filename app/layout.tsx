04da348a8239e527776f753d18a1808e7d8acc11
Agni 🔥Sarvaloka ✨ <tmwgc@proton.me>
2024-09-13 01:40:17 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,9 +6,9 @@ import AppLayout from './appLayout';
 const inter = Plus_Jakarta_Sans({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'TMWGC | Digital Strategies for Growth Stage Enterprises',
+  title: 'Mayer Insurance Agency | Leading Professional Insurance Consulting Agency in United States',
   description:
-    'We’re data-driven strategists delivering growth optimization approaches to unlock the channelize the fullswing potential of revenue streams in your business. Madras Wallstreet Growth Consulting, envisions to enable regional brands to achieve global reach through proven digital strategies.',
+    'We're the best professional insurance consulting agency for all needs. We offer exclusive consultations and services that protect you, your beloved, and your belongings at all costs, to keep you protected and hassle-free.',
 };
 
 export default function RootLayout({
