b910f6574472eb34308ef1c2c9adc928615244b7
Agni 🔥Sarvaloka ✨ <tmwgc@proton.me>
2024-09-13 01:42:57 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,7 @@ const inter = Plus_Jakarta_Sans({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Mayer Insurance Agency | Leading Professional Insurance Consulting Agency in United States',
   description:
-    'We're the best professional insurance consulting agency for all needs. We offer exclusive consultations and services that protect you, your beloved, and your belongings at all costs, to keep you protected and hassle-free.',
+    'We are the best professional insurance consulting agency for all needs. We offer exclusive consultations and services that protect you, your beloved, and your belongings at all costs, to keep you protected and hassle-free.',
 };
 
 export default function RootLayout({
